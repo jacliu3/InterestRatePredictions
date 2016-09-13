ec490161907917317543a775b05a8961410d9abf
Jacqueline Liu <liu.jacqueline@berkeley.edu>
2016-09-13 04:09:08 -0700

    Added linear regression model

--- models/LRmodel.py
+++ models/LRmodel.py
@@ -1,10 +1,7 @@
 import numpy as np
 from sklearn import cross_validation
-from sklearn.svm import SVR
-from sklearn import grid_search
 from sklearn.externals import joblib
-from sklearn.linearmodel import LogisticRegression
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.linear_model import LinearRegression
 
 # Load data from numpy array 
 data_trans = np.load('C:\\Users\\Jacqueline\\Documents\\Dropbox\\SFworksample\\data\\clean_data.npy')
@@ -18,18 +15,13 @@ cross_validation.train_test_split(data_trans, data_labels, random_state=6)
 def rmse(pred_labels, true_labels):
     return np.sqrt(((pred_labels - true_labels) ** 2).mean())
 
-selector = VarianceThreshold()
-selector.fit(data_train)
-selector.variances_
-
-# LET'S MODEL SOME MORE
-svr = SVR()
-svr.fit(data_train, label_train)
-rmse(svr.predict(data_train), label_train)
-rmse(svr.predict(data_test), label_test)
-
 # ALLLL THE MODELS
-lr = LogisticRegression()
-ls.fit(data_train, label_train)
-rmse(lr.predict(data_train), label_train)
-rmse(svr.predict(data_test), label_test)
+# JK. Saddest thing of my life
+# After all that time spent with the SVRs, KernelRidge, etc...
+lr = LinearRegression()
+lr.fit(data_train, label_train)
+best = rmse(lr.predict(data_test), label_test)
+
+# Save
+joblib.dump(lr, 'C:\\Users\\Jacqueline\\Documents\\Dropbox\\SFworksample\\models\\lr.pkl')
+
